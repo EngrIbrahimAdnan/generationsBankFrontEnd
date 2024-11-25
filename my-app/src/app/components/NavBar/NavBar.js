bf935d55e9e0f5fd17cd5baf584219e62ee409cb
Abdulrahman Alfahad <abdulrahman.g.alfahad@gmail.com>
2024-11-25 12:19:33 +0300

    inititlized shadCn and created global.css

--- my-app/src/app/components/NavBar/NavBar.js
+++ my-app/src/app/components/NavBar/NavBar.js
@@ -1,26 +1,41 @@
 import React from "react";
 import Link from "next/link";
 import routes from "../../constants/routes";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
 
 function NavBar() {
   return (
-    <div>
-      <Link href={routes.home}>
-        <div>Home</div>
-      </Link>
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+    // <div>
+    //   <Link href={routes.home}>
+    //     <div>Home</div>
+    //   </Link>
 
-      <Link href={routes.login}>
-        <div>Login</div>
-      </Link>
+    //   <Link href={routes.login}>
+    //     <div>Login</div>
+    //   </Link>
 
-      <Link href={routes.register}>
-        <div>Register</div>
-      </Link>
+    //   <Link href={routes.register}>
+    //     <div>Register</div>
+    //   </Link>
 
-      <Link href={routes.about}>
-        <div>About</div>
-      </Link>
-    </div>
+    //   <Link href={routes.about}>
+    //     <div>About</div>
+    //   </Link>
+    // </div>
   );
 }
 
